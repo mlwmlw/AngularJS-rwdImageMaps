0cb74d5614a83380c0a18dea5b56105692c3b50d
ShangWei Lin <shangwei.lin@kuobrothers.com>
2014-07-30 10:23:49 +0800

    Remove jQuery dependency

--- angular-rwdImageMaps.js
+++ angular-rwdImageMaps.js
@@ -11,24 +11,29 @@
 * angular-rwdImageMaps.js (by Philip Saa)
 * https://github.com/cowglow/
 * @cowglow
+*
+* http://mlwmlw.org
+* Remove jQuery dependency
+* @mlwmlw
 */
 
 angular.module('rwdImageMaps',[])
 	.directive('rwdimgmap', function($window){
 		return{
 			restrict: 'CA',
-			link:function(scope, element, attrs){
+			link:function(scope, element, attrs) {
+
 				element.bind('load', function() {
 
-					var w = $(element).attr('width'),
-						h = $(element).attr('height');
-						
-					function resize(){
+					var w = element[0].naturalWidth,
+						h = element[0].naturalHeight,
+						origin = [];
+					function resize() {
 						if (!w || !h) {
 							var temp = new Image();
-							temp.src = $(element).attr('src');
+							temp.src = element[0].src;
 							if(temp.src == undefined)
-								temp.src = $(element).attr('ng-src');
+								temp.src = element[0].ng-src;
 
 							if (!w)
 								w = temp.width;
@@ -36,32 +41,31 @@ angular.module('rwdImageMaps',[])
 								h = temp.height;
 						}
 						
-						var wPercent = $(element).width()/100,
-							hPercent = $(element).height()/100,
+						var wPercent = element[0].width/w,
+							hPercent = element[0].height/h,
 							map = attrs.usemap.replace('#', ''),
 							c = 'coords';
-						
-						angular.element('map[name="' + map + '"]').find('area').each(function(){
-							var $this = $(this);
-							
-							if (!$this.data(c)){
-								$this.data(c, $this.attr(c));
+						angular.forEach(angular.element(document.getElementsByName(map)).find('area'), function(area, i){
+							if (!area[c]){
+								return;
 							}
-								
-							var coords = $this.data(c).split(','),
+							if(!origin[i])
+								origin[i] = area[c];
+
+							var coords = origin[i].split(','),
 								coordsPercent = new Array(coords.length);
 							
 							for (var i = 0; i<coordsPercent.length; ++i){
 								if (i % 2 === 0){
-									coordsPercent[i] = parseInt(((coords[i]/w)*100)*wPercent);
+									coordsPercent[i] = parseInt(coords[i] * wPercent);
 								} else {
-									coordsPercent[i] = parseInt(((coords[i]/h)*100)*hPercent);
+									coordsPercent[i] = parseInt(coords[i] * hPercent);
 								};
 							};
-							$this.attr(c, coordsPercent.toString());
+							area[c] = coordsPercent.toString();
 						});
 					}
-					angular.element($window).resize(resize).trigger('resize');
+					angular.element($window).bind('resize', resize).triggerHandler('resize');
 				});
 			}
 		};
